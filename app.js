40d59cd3973c68a2ee4859e1cbce4e993b48ecf8
Neelkanth Kaushik <neelkanthkaushik2014@gmail.com>
2019-03-25 00:25:28 +0530

    joi module

--- app.js
+++ app.js
@@ -1,32 +1,121 @@
 const express = require('express');
+const Joi = require('joi');
 const vidflixDb = require('./vidflixDb');
 const vidflixApi = express();
+
 const moviesList = vidflixDb.movies;
+
 var i = 1;
 for (var i in moviesList) {
-    moviesList[i].id = parseInt(i)+1;
+    moviesList[i].id = parseInt(i) + 1;
 }
 
 //Get all movies
 vidflixApi.get('/api/v1/movies', (req, res) => {
-    let data = {
+    let movies = {
         "total": moviesList.length,
-        "data": moviesList.slice(0,9)
+        "movies": moviesList.slice(0, 9)
     };
-    res.status(200).contentType('application/json').send(data);
+    res.status(200).contentType('application/json').send(movies);
 });
 //Get movie by Id
 vidflixApi.get('/api/v1/movies/:id', (req, res) => {
     var id = parseInt(req.params.id); //Type casting 'id' into integer
     let queryParams = req.query;
     //res.send(queryParams);
-    let data = moviesList.find(movie => movie.id === id);
+    const movie = moviesList.find(movie => movie.id === id);
 
-    if(!data)
-        res.status(404);
-
-    res.send(data);
+    if (!movie)
+    {
+        res.status(404).send("Movie with given ID is not found."); //404 Not Found
+        return;
+    }
+    res.send(movie);
 });
+
+//Adding a new movie
+vidflixApi.use(express.json()); //Middleware to parse request body
+
+vidflixApi.post('/api/v1/movies', (req, res) => {
+    const validationResult = validateMovie(req.body);
+    if (validationResult.error) {
+        res.status(400).send(validationResult.error.details); //400 Bad Request
+        return;
+    }
+
+    const newMovie = {
+        "id": moviesList.length + 1,
+        "title": req.body.title,
+        "year": req.body.year,
+        "genres": req.body.genres,
+        "ratings": req.body.ratings,
+        "poster": req.body.poster,
+        "contentRating": req.body.contentRating,
+        "duration": req.body.duration,
+        "releaseDate": req.body.releaseDate,
+        "averageRating": req.body.averageRating,
+        "originalTitle": req.body.originalTitle,
+        "storyline": req.body.storyline,
+        "actors": req.body.actors,
+        "imdbRating": req.body.imdbRating,
+        "posterurl": req.body.posterurl
+    };
+    moviesList.push(newMovie);
+    //return created entity
+    res.status(200).send(newMovie);
+});
+
+vidflixApi.put('/api/v1/movies/:id', (req, res) => {
+    var id = parseInt(req.params.id); //Type casting 'id' into integer
+    const movie = moviesList.find(movie => movie.id === id);
+    if (!movie){
+        res.status(404).send("Movie with given ID is not found."); //404 Not Found
+        return;
+    }
+    const validationResult = validateMovie(req.body);
+    if (validationResult.error) {
+        res.status(400).send(validationResult.error.details); //400 Bad Request
+        return;
+    }
+    //Update
+    movie.title = req.body.title;
+    res.send(movie);
+});
+
+vidflixApi.delete('/api/v1/movies/:id', (req, res) => {
+    var id = parseInt(req.params.id); //Type casting 'id' into integer
+    const movie = moviesList.find(movie => movie.id === id);
+    if (!movie){
+        res.status(404).send("Movie with given ID is not found."); //404 Not Found
+        return;
+    }
+    //Delete
+    let index = moviesList.indexOf(movie);
+    moviesList.splice(index, 1);
+    res.send(movie);
+});
+
+function validateMovie(movie) {
+    //validation using Joi
+    const validationSchema = {
+        title: Joi.string().required(),
+        year: Joi.allow(),
+        genres: Joi.allow(),
+        ratings: Joi.allow(),
+        poster: Joi.allow(),
+        contentRating: Joi.allow(),
+        duration: Joi.allow(),
+        releaseDate: Joi.allow(),
+        averageRating: Joi.allow(),
+        originalTitle: Joi.allow(),
+        storyline: Joi.allow(),
+        actors: Joi.allow(),
+        imdbRating: Joi.allow(),
+        posterurl: Joi.allow()
+    };
+
+    return Joi.validate(movie, validationSchema);
+}
 
 const port = process.env.PORT || 3000;
 vidflixApi.listen(port, () => {
